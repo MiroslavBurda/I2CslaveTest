4978aad9630308dab26773c7854ec19f578f05ac
MiroslavBurda <35538660+MiroslavBurda@users.noreply.github.com>
2022-04-01 18:00:25 +0200

    succesfully working with EV3

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,7 @@
 
 gpio_num_t sda_pin = GPIO_NUM_18;
 gpio_num_t scl_pin = GPIO_NUM_19; 
-// uint32_t speed_hz = 100000;
-uint8_t address = 0x15;
+uint8_t address = 0x04;
 i2c_port_t bus_num = I2C_NUM_0; 
 uint8_t DataToReceive[] = {2, 2, 2, 2};
 size_t len = sizeof(DataToReceive);
@@ -34,11 +33,13 @@ i2c_config_t conf_slave = {
 };
 
 int i = 0; 
+int written = 0;
 
 extern "C" void app_main() {  // example for connect ESP32 with I2C
     ESP_ERROR_CHECK(i2c_param_config(bus_num, &conf_slave));
     ESP_ERROR_CHECK(i2c_driver_install(bus_num, I2C_MODE_SLAVE, 2000, 2000, 0)); 
-    i2c_slave_write_buffer(bus_num, DataToSend, 8, pdMS_TO_TICKS(25)); 
+    written = i2c_slave_write_buffer(bus_num, DataToSend, 8, pdMS_TO_TICKS(25)); 
+    ESP_LOGI("WRITTEN:", "%i \n", written);  
 
     while (true) {
         i2c_slave_read_buffer(bus_num, DataToReceive, 4, pdMS_TO_TICKS(25)); 
